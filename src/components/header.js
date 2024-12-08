d18e58f3bb06771673187879febb5dde656df2ba
Matias Aguirre <matiasezeaguirre@gmail.com>
2024-12-08 13:05:34 -0300

    creacion de componente header.js

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+export default function header() {
+    return (
+        <>
+        {/* Header */}
+        <header className="flex items-center justify-between p-4 bg-white border-b shadow-md">
+            <div className="flex items-center">
+                <img src="/assets/logo.png" alt="Logo"/>
+            </div>
+        </header>
+        </>
+    );
+}
